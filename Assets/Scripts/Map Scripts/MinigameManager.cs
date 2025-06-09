2f48f84b9ef14810e8d557e7bb61b3517fd483b8
keegenwall-is <keegen.wallis@gmail.com>
2025-06-10 00:11:57 +1000

    UI Timer Text

--- Assets/Scripts/Map Scripts/MinigameManager.cs
+++ Assets/Scripts/Map Scripts/MinigameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class MinigameManager : MonoBehaviour
 {
     public List<GameObject> players = new List<GameObject>();
     public int playerNo;
     public List<GameObject> effectedObjects = new List<GameObject>();
+    public Text countdown;
+    public float gameLength;
 
     private GameController gameController;
+    private bool roundOver = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,7 +26,19 @@ public class MinigameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!roundOver)
+        {
+            if (gameLength >= 0)
+            {
+                gameLength -= Time.deltaTime;
+                TimeSpan timeText = TimeSpan.FromSeconds(gameLength);
+                countdown.text = string.Format("{0}:{1:00}", timeText.Minutes, timeText.Seconds);
+            }
+            else
+            {
+                roundOver = true;
+            }
+        }
     }
 
     public void TriggerObstacleEvent(GameObject actor)
@@ -32,5 +49,10 @@ public class MinigameManager : MonoBehaviour
     protected virtual void OnObstacleEvent(GameObject actor)
     {
         //Default is to do nothing (obstacles don't have the same effect in all minigames)
+    }
+
+    protected virtual void OnMinigameEnd()
+    {
+        //Show end game screen
     }
 }
