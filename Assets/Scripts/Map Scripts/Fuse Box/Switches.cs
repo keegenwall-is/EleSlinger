1cadc036d90e5ef919750250b70160d6125d3b2e
keegenwall-is <keegen.wallis@gmail.com>
2025-06-18 10:25:40 +1000

    Switch animations

---
+++ Assets/Scripts/Map Scripts/Fuse Box/Switches.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    public float currentLength;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Current"))
+        {
+            anim.SetBool("Switch On", true);
+            StartCoroutine(SwitchOff());
+        }
+    }
+
+    private IEnumerator SwitchOff()
+    {
+        yield return new WaitForSeconds(currentLength - 0.5f);
+        anim.SetBool("Switch On", false);
+    }
+}
