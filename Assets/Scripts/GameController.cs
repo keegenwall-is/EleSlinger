ea6f6cad0a95473fbbbab8645c83692527ad56f1
keegenwall-is <keegen.wallis@gmail.com>
2025-08-12 13:20:55 +1000

    Reinstating tutorial

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -102,7 +102,7 @@ public class GameController : MonoBehaviour
 
     public void LoadTutorial()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(1);
     }
 
     public void LoadRandomMinigame()
