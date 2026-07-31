56e730c440195d63d73d26fa97eb154b5cd9c358
LudensOCE <keegen.wallis@gmail.com>
2026-07-31 15:21:40 +1000

    Melee CD + input buffer on dash

--- Assets/Scripts/Player Scripts/PlayerMelee.cs
+++ Assets/Scripts/Player Scripts/PlayerMelee.cs
@@ -6,11 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMelee : MonoBehaviour
 {
 
-    private CharacterBase baseScript;
-    private GameObject thisMelee;
-
     public GameObject melee;
     public GameObject meleeSpawner;
+    public float meleeCD = 0.5f;
+
+    private CharacterBase baseScript;
+    private GameObject thisMelee;
+    private float meleeCurrent;
+    private bool inputBuffer;
 
     // Start is called before the first frame update
     void Start()
@@ -21,42 +24,72 @@ public class PlayerMelee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Checks the kind of controller the player is using
-        if (baseScript.thisController is Keyboard keyboard)
+        meleeCurrent -= Time.deltaTime;
+        if (meleeCurrent <= 0)
         {
-            //Starts the attack if the player is idle or running and presses the attack button
-            if (baseScript.GetState() == CharacterBase.playerState.Idle || baseScript.GetState() == CharacterBase.playerState.Running)
+            meleeCurrent = 0;
+            //Checks the kind of controller the player is using
+            if (baseScript.thisController is Keyboard keyboard)
             {
+                //Starts the attack if the player is idle or running and presses the attack button
                 if (keyboard.oKey.wasPressedThisFrame)
                 {
-                    baseScript.SetState(CharacterBase.playerState.Melee);
-                    thisMelee = Instantiate(melee, meleeSpawner.transform);
-                    MeleeBehaviour meleeScript = thisMelee.GetComponent<MeleeBehaviour>();
-                    meleeScript.SetThrower(gameObject);
-                    StartCoroutine(EndMelee());
+                    if (baseScript.GetState() == CharacterBase.playerState.Idle || baseScript.GetState() == CharacterBase.playerState.Running)
+                    {
+                        Melee();
+                    }
+                    else if (baseScript.GetState() == CharacterBase.playerState.Dashing)
+                    {
+                        inputBuffer = true;
+                    }
                 }
+                
             }
-        }
-        else if (baseScript.thisController is Gamepad controller)
-        {
-            if (baseScript.GetState() == CharacterBase.playerState.Idle || baseScript.GetState() == CharacterBase.playerState.Running)
+            else if (baseScript.thisController is Gamepad controller)
             {
                 if (controller.buttonNorth.wasPressedThisFrame)
                 {
-                    baseScript.SetState(CharacterBase.playerState.Melee);
-                    thisMelee = Instantiate(melee, meleeSpawner.transform);
-                    MeleeBehaviour meleeScript = thisMelee.GetComponent<MeleeBehaviour>();
-                    meleeScript.SetThrower(gameObject);
-                    StartCoroutine(EndMelee());
+                    if (baseScript.GetState() == CharacterBase.playerState.Idle || baseScript.GetState() == CharacterBase.playerState.Running)
+                    {
+                        Melee();
+                    }
+                    else if (baseScript.GetState() == CharacterBase.playerState.Dashing)
+                    {
+                        inputBuffer = true;
+                    }
+                }
+            }
+
+            if (inputBuffer)
+            {
+                if ((baseScript.GetState() == CharacterBase.playerState.Idle || baseScript.GetState() == CharacterBase.playerState.Running) && meleeCurrent <= 0)
+                {
+                    inputBuffer = false;
+                    Melee();
                 }
             }
         }
+        
+    }
+
+    private void Melee()
+    {
+        baseScript.SetState(CharacterBase.playerState.Melee);
+        thisMelee = Instantiate(melee, meleeSpawner.transform);
+        MeleeBehaviour meleeScript = thisMelee.GetComponent<MeleeBehaviour>();
+        meleeScript.SetThrower(gameObject);
+        StartCoroutine(EndMelee());
+        meleeCurrent = meleeCD;
     }
 
     private IEnumerator EndMelee()
     {
         yield return new WaitForSeconds(0.3f);
-        if (baseScript.GetState() != CharacterBase.playerState.Dead && baseScript.GetState() != CharacterBase.playerState.TakingHit && baseScript.GetState() != CharacterBase.playerState.Stunned)
+        //if (baseScript.GetState() != CharacterBase.playerState.Dead && baseScript.GetState() != CharacterBase.playerState.TakingHit && baseScript.GetState() != CharacterBase.playerState.Stunned)
+        //{
+        //    baseScript.SetState(CharacterBase.playerState.Idle);
+        //}
+        if (baseScript.GetState() == CharacterBase.playerState.Melee)
         {
             baseScript.SetState(CharacterBase.playerState.Idle);
         }
